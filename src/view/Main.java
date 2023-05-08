62f0a8e8cff281e10175f25e4888b4d7eae647b0
Dougkid33 <douglasaugustomsilva@hotmail.com>
2023-05-08 15:30:13 -0300

    ajusteMenu

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,5 +1,6 @@
 package view;
 
+
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -14,94 +15,102 @@ import controller.ProcedimentoController;
 import controller.UnidadeController;
 import model.Pessoa;
 
+
+
+
 public class Main {
+	
 
-    Pessoa pessoa = new Pessoa("Administrador", "Xablau", "validador", "33334380", "admin", "12345", "DonoFranquia", null, null);
-    Pessoa user = new Pessoa("user", "Xablau", "166496466", "33334380", "user", "333", "Paciente", null, null);
-    
-    public void menuPrincipal(long id) {
-    }
-    // MENU INICIO
+	Pessoa pessoa = new Pessoa( "Administrador", "Xablau", "validador", "33334380", "admin", "12345", "DonoFranquia", null, null);
+	
 
-    @SuppressWarnings("null")
-    public boolean login() {
-        Scanner sc = new Scanner(System.in);
+	public void menuPrincipal(long id) {
 
-        System.out.println("==============================================");
-        System.out.println("Sistema de Gerenciamento de Hospitais - Login");
-        System.out.println("=============================================\n");
+	}
 
-        String usuario;
-        do {
-            System.out.print("Usuário: ");
-            usuario = sc.nextLine().trim();
-            if (usuario.isEmpty()) {
-                System.out.println("Usuário inválido. Tente novamente.");
-            }
-        } while (usuario.isEmpty());
+	// MENU INICIO
+	@SuppressWarnings("null")
+	public boolean login() {
+	    Scanner scanner = new Scanner(System.in);
 
-        String senha;
-        do {
-            System.out.print("Senha: ");
-            senha = sc.nextLine().trim();
-            if (senha.isEmpty()) {
-                System.out.println("Senha inválida. Tente novamente.");
-            }
-        } while (senha.isEmpty());
+	    System.out.println("======================================");
+	    System.out.println("Sistema de Gerenciamento de Hospitais - Login");
+	    System.out.println("======================================\n");
 
-        try {
-            // this.nomeDeUsuarioPadrao == "admin" && this.senhaPadrao == "12345"
-            PessoaController controller = new PessoaController();
-            //Pessoa pessoa = new Pessoa( "Administrador", "Xablau", "validador", "33334380", "admin", "12345", "DonoFranquia", null, null);
-            controller.cadastrarPessoa(this.pessoa);
-            System.out.println("Este é seu ID: " + pessoa.getId() + " .");
-            if (pessoa != null && pessoa.getSenha().equals("12345")) {
-                menuPrincipal(pessoa.getId());
-                exibirMenu();
-                return true;
-            } else {
-                System.out.println("\n Usuário ou senha inválidos. Tente novamente.\n");
-                return false;
-            }
-        } catch (Exception e) {
-            System.out.println("\n Erro ao realizar o login. Tente novamente mais tarde.\n");
-            return false;
-        } finally {
-            sc.close();
-        }
-    }
+	    String usuario;
+	    do {
+	        System.out.print("Usuário: ");
+	        usuario = scanner.nextLine().trim();
+	        if (usuario.isEmpty()) {
+	            System.out.println("Usuário inválido. Tente novamente.");
+	        }
+	    } while (usuario.isEmpty());
 
-    public static void exibirMenu() {
-        boolean sair = false;
+	    String senha;
+	    do {
+	        System.out.print("Senha: ");
+	        senha = scanner.nextLine().trim();
+	        if (senha.isEmpty()) {
+	            System.out.println("Senha inválida. Tente novamente.");
+	        }
+	    } while (senha.isEmpty());
 
-        while (!sair) {
-            StringBuilder builder = new StringBuilder();
+	    try {
+	    	// this.nomeDeUsuarioPadrao == "admin" && this.senhaPadrao == "12345"
+	    	PessoaController controller = new PessoaController();
+	    	//Pessoa pessoa = new Pessoa( "Administrador", "Xablau", "validador", "33334380", "admin", "12345", "DonoFranquia", null, null);
+	    	controller.cadastrarPessoa(this.pessoa);
+	    	System.out.println("Este é seu ID: " + pessoa.getId()+" .");
+	    	if (pessoa != null && pessoa.getSenha().equals("12345")) {
+	    	    menuPrincipal(pessoa.getId());
+	    	    exibirMenu();
+	    	    return true;
+	    	} else {
+	            System.out.println("\n Usuário ou senha inválidos. Tente novamente.\n");
+	            return false;
+	        }
+	    } catch (Exception e) {
+	        System.out.println("\n Erro ao realizar o login. Tente novamente mais tarde.\n");
+	        return false;
+	    } finally {
+	        scanner.close();
+	    }
+	}
 
-            builder.append("\n\n\n\n\n\n\n =============================================================\n");
-            builder.append(" Sistema de Gerenciamento de Clínicas Médicas \n");
-            builder.append(" ========================================================/Home\n");
-            builder.append("\n Escolha a opção desejada:\n");
-            builder.append("1. Pessoa \n");
-            builder.append("2. Médico \n");
-            builder.append("3. Franquia \n");
-            builder.append("4. Unidade de Franquia \n");
-            builder.append("5. Consulta \n");
-            builder.append("6. Informação de Consulta \n");
-            builder.append("7. Procedimento \n");
-            builder.append("8. Financeiro ADM \n");
-            builder.append("9. Financeiro Médico \n");
-            builder.append("10. Relatórios Financeiros \n");
-            builder.append("0. Sair \n");
-            builder.append("------------------------------------ \n");
+	public static void exibirMenu() {
+		boolean sair = false;
 
-            try (Scanner scanner = new Scanner(System.in)) {
-                int opcao = -1;
-                try {
-                    opcao = scanner.nextInt();
-                } catch (InputMismatchException e) { // valida a opcao
-                    System.out.println("Opção inválida. Tente novamente.");
-                    continue; // volta ao início do loop
-                }
+		while (!sair) {
+			StringBuilder builder = new StringBuilder();
+
+			builder.append("\n\n\n\n\n\n\n =============================================================\n");
+			builder.append(" Sistema de Gerenciamento de Clínicas Médicas \n");
+			builder.append(" ========================================================/Home\n");
+			builder.append("\n Escolha a opção desejada:\n");
+			builder.append("1. Pessoa \n");
+			builder.append("2. Médico \n");
+			builder.append("3. Franquia \n");
+			builder.append("4. Unidade de Franquia \n");
+			builder.append("5. Consulta \n");
+			builder.append("6. Informação de Consulta \n");
+			builder.append("7. Procedimento \n");
+			builder.append("8. Financeiro ADM \n");
+			builder.append("9. Financeiro Médico \n");
+			builder.append("10. Relatórios Financeiros \n");
+			builder.append("0. Sair \n");
+			builder.append("------------------------------------ \n");
+
+			System.out.println(builder.toString());
+
+			try (Scanner scanner = new Scanner(System.in)) {
+				int opcao = -1;
+				try {
+					opcao = scanner.nextInt();
+				} catch (InputMismatchException e) { // valida a opcao
+					System.out.println("Opção inválida. Tente novamente.");
+					continue; // volta ao início do loop
+				}
+
                 switch (opcao) {
                     case 0:
                         System.out.println("Saindo...");
@@ -137,11 +146,11 @@ public class Main {
                         break;
                     case 8:
                         System.out.println("Selecionou Financeiro ADM.");
-                        FinanceiroADMController.menuFinanceiroADM();
+                       // FinanceiroADMController.menuFinanceiroADM();
                         break;
                     case 9:
                         System.out.println("Selecionou Financeiro Médico.");
-                        FinanceiroMedicoController.menuFinanceiroMedico();
+                        //FinanceiroMedicoController.menuFinanceiroMedico();
                         break;
                     case 10:
                         System.out.println("Selecionou Relatórios Financeiros.");
@@ -151,18 +160,19 @@ public class Main {
                         System.out.println("Opção inválida. Tente novamente.");
                         break;
                 }
-            }
-        }
-    }
-    public static void main(String[] args) {
+			}
+		}
+	}
 
-        // verificacao do login para entrar no menuPrincipal
-        Main main = new Main();
-        boolean loginValido = main.login();
-        if (loginValido) {
-            exibirMenu();
-        }
+	public static void main(String[] args) {
 
-    }
+		// verificacao do login para entrar no menuPrincipal
+		Main main = new Main();
+		boolean loginValido = main.login();
+		if (loginValido) {
+			exibirMenu();
+		}
+
+	}
 
 }
