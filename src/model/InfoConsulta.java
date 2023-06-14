a2acefff574c45baa8b5d59168ce6189ba01a7ae
MurilodioPy <engmurilo@edu.uniube.br>
2023-06-14 16:37:03 -0300

    edit infoConsulta.java

--- src/model/InfoConsulta.java
+++ src/model/InfoConsulta.java
@@ -1,6 +1,6 @@
 package model;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.Random;
 
 import model.enums.EstadoConsulta;
@@ -9,11 +9,12 @@ public class InfoConsulta extends Consulta {
 
     private int id;
     private String descricao;
-    private Date dataCriacao;
-    private Date dataModificacao;
+    private LocalDateTime dataCriacao;
+    private LocalDateTime dataModificacao;
 
-    public InfoConsulta(int id, Date data, String hora, EstadoConsulta estado, Medico medico, Pessoa paciente, double valor, Unidade unidade, String descricao, Date dataCriacao, Date dataModificacao) {
-        super(id, data, hora, estado, medico, paciente, valor, unidade, dataModificacao, dataModificacao);
+    public InfoConsulta(int id, LocalDateTime data, String hora, EstadoConsulta estado, Medico medico, 
+            Pessoa paciente, double valor, Unidade unidade, String descricao, LocalDateTime dataCriacao, LocalDateTime dataModificacao) {
+        super(data, hora, estado, medico, paciente, valor, unidade, dataModificacao, dataModificacao);
         this.id = gerarNovoId();
         this.descricao = descricao;
         this.dataCriacao = dataCriacao;
@@ -30,16 +31,17 @@ public class InfoConsulta extends Consulta {
 
     public void setDescricao(String descricao) {
         this.descricao = descricao;
-        this.dataModificacao = new Date();
+        this.dataModificacao = LocalDateTime.now();
     }
 
-    public Date getDataCriacao() {
+    public LocalDateTime getDataCriacao() {
         return dataCriacao;
     }
 
-    public Date getDataModificacao() {
+    public LocalDateTime getDataModificacao() {
         return dataModificacao;
     }
+    
     private static int proximoId = 1;
 
     private static int gerarNovoId() {
@@ -51,18 +53,19 @@ public class InfoConsulta extends Consulta {
 
         Random random = new Random();
 
-        Date dataConsulta = new Date(System.currentTimeMillis() + (random.nextInt(30) + 1) * 24 * 60 * 60 * 1000); // Gera uma data aleatória nos próximos 30 dias
+        LocalDateTime dataConsulta = LocalDateTime.now().plusDays(random.nextInt(30) + 1); // Gera uma data aleatória nos próximos 30 dias
         String horaConsulta = horas[random.nextInt(horas.length)];
         EstadoConsulta estadoConsulta = EstadoConsulta.values()[random.nextInt(EstadoConsulta.values().length)]; // Gera um estado aleatório a partir dos valores possíveis na enumeração EstadoConsulta
         Medico medico = Medico.gerarMedicoAleatorio();
         Pessoa paciente = Pessoa.gerarPessoaAleatoria();
         double valorConsulta = random.nextDouble() * 300; // Gera um valor aleatório entre 0 e 300
 
-        Unidade unidade = Unidade.gerarUnidadeAleatoria();
+        Unidade unidade = new Unidade();
+                //Unidade.gerarUnidadeAleatoria();
 
         String descricao = "Descrição da consulta " + gerarNovoId();
-        Date dataCriacao = new Date();
-        Date dataModificacao = new Date();
+        LocalDateTime dataCriacao = LocalDateTime.now();
+        LocalDateTime dataModificacao = LocalDateTime.now();
 
         InfoConsulta infoConsulta = new InfoConsulta(gerarNovoId(), dataConsulta, horaConsulta, estadoConsulta, medico, paciente, valorConsulta, unidade, descricao, dataCriacao, dataModificacao);
 
